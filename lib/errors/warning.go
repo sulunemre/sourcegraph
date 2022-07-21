590f69c730c491b1ba2a186692deb3ff74ac77df
Indradhanush Gupta <indradhanush.gupta@gmail.com>
2022-07-22 00:07:54 +0530

    WIP

--- lib/errors/warning.go
+++ lib/errors/warning.go
@@ -15,6 +15,21 @@ type warning struct {
 // Ensure that warning always implements the Warning error interface.
 var _ Warning = (*warning)(nil)
 
+var WarningReference = warning{}
+
+// NewWarningError will return an error of type warning. This should be used to wrap errors where we
+// do not intend to return an error, increment an error metric. That is, if an error is returned and
+// it is not critical and / or expected to be intermittent and / or nothing we can do about
+// (example: 404 errors from upstream code host APIs in repo syncing), we should wrap the error with
+// NewWarningError.
+//
+// Consumers of these errors should then use errors.Is to check if the error is of a warning type
+// and based on that, should just log it as a warning. For example:
+//
+// TODO
+// err := someFunctionThatReturnsAWarningErrorOrACriticalError()
+// TODO
+//
 func NewWarningError(err error) error {
 	return &warning{
 		Err: err,
--- lib/errors/warning_test.go
+++ lib/errors/warning_test.go
@@ -10,10 +10,14 @@ func TestWarningError(t *testing.T) {
 	err := errors.New("foo")
 	w := NewWarningError(err)
 	if _, ok := w.(Warning); !ok {
-		t.Error(`Expected variable "w" to be of type Warning`)
+		t.Error(`Expected variable "w" to be of type warning`)
 	}
 
 	if errors.Is(err, &warning{}) {
-		t.Error(`Expected variable "err" to not be of type Warning`)
+		t.Error(`Expected variable "err" to NOT be of type warning`)
+	}
+
+	if !errors.Is(w, &WarningReference) {
+		t.Error(`Expected variable "err" to be of type warning`)
 	}
 }
